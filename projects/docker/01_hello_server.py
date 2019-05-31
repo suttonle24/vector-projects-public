28663d5a9dffc423a861db786b29c69d73e312c5
Lee Sutton <leensutton@outlook.com>
2019-05-31 06:06:26 -0700

    Remove name-specific robot target

--- projects/docker/01_hello_server.py
+++ projects/docker/01_hello_server.py
@@ -24,7 +24,7 @@ import anki_vector
 
 def main():
     args = anki_vector.util.parse_command_args()
-    with anki_vector.Robot(name="Vector-Z1W6") as robot:
+    with anki_vector.Robot(args.serial) as robot:
         print("Say 'Hello Server'...")
         robot.behavior.say_text("Hello Server")
 
